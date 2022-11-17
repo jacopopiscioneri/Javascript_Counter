1accfe38be19d1400dcfee9e4adc47301dae573f
Jacopo Piscioneri <78322277+jacopopiscioneri@users.noreply.github.com>
2022-11-17 22:59:05 +0100

    reset function
    Reset function added to the code

--- Counter/Javascript/main.js
+++ Counter/Javascript/main.js
@@ -1,12 +1,17 @@
 let add = document.getElementById('plus');
 let remove = document.getElementById('minus');
 let counter = document.getElementById('number');
+let reset = document.getElementById('reset');
 let integer = 0;
-add.addEventListener('click', function(){
+add.addEventListener('click', function increment(){
   integer +=1;
   counter.innerHTML = integer;
 })
-remove.addEventListener('click', function(){
+remove.addEventListener('click', function decrement(){
   integer -=1;
   counter.innerHTML = integer;
+})
+reset.addEventListener('click', function reset(){
+    integer = 0;
+    counter.innerHTML = integer;
 })
